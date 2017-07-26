b28e33d5f8f7e6994ad5582b2186149f92098668
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-07-26 17:47:19 +0530

    Create FibFromPascal.py

---
+++ FibFromPascal.py
@@ -0,0 +1,20 @@
+def fib_pascal(n,fib_pos):
+    if n == 1:
+        line = [1]
+        fib_sum = 1 if fib_pos == 0 else 0
+    else:
+        line = [1]
+        (previous_line, fib_sum) = fib_pascal(n-1, fib_pos+1)
+        for i in range(len(previous_line)-1):
+            line.append(previous_line[i] + previous_line[i+1])
+        line += [1]
+        if fib_pos < len(line):
+            fib_sum += line[fib_pos]
+    return (line, fib_sum)
+
+def fib(n):
+    return fib_pascal(n,0)[1]
+
+# and now printing out the first ten Fibonacci numbers:
+for i in range(1,10):
+    print(fib(i))
