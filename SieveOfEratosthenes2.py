66f606ddbc6f5f74955290ed50211d2775e1e534
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-07-26 17:56:53 +0530

    Create SieveOfEratosthenes2.py

---
+++ SieveOfEratosthenes2.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+def primes(n):
+    if n == 0:
+        return []
+    elif n == 1:
+        return []
+    else:
+        p = primes(int(sqrt(n)))
+        no_p = [j for i in p for j in range(i*2, n + 1, i)]
+        p = [x for x in range(2, n + 1) if x not in no_p]
+        return p
+
+num = int(input("enter N:"))
+print(primes(num))
