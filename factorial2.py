3b8473f0fb92814c16a0ff36fb6cb4baccb2ff11
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-07-26 16:47:26 +0530

    Create factorial2.py

---
+++ factorial2.py
@@ -0,0 +1,11 @@
+'''
+recursive factorial program taking user inputs
+'''
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        res = n * factorial(n-1)
+        return res	
+num = (int)input("Enter a number:")
+print(factorial(num))
